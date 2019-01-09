d92d843510022ee93ea73ba1a1887cc1e4ac6366
WeStandUnited <45972107+WeStandUnited@users.noreply.github.com>
2019-01-09 11:46:50 -0500

    TextMeshPro Score (Working)

---
+++ Brain Jam/Assets/TextMeshProScore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextMeshProScore : MonoBehaviour {
+
+ public TMPro.TextMeshProUGUI Score_Text;
+	ScoreScript score = new ScoreScript();
+
+ void Update () {
+
+ if (score.getScore() != null){
+	 Score_Text.text = "Score: " + score.getScore();
+ }else {
+		 Score_Text.text = "Score: "+ 0;
+
+ }
+ }
+
+}
