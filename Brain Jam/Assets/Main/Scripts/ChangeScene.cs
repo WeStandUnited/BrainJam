78248afa89afe45af0a4d3150006e9ba584e6062
WeStandUnited <45972107+WeStandUnited@users.noreply.github.com>
2019-02-27 11:49:38 -0500

    UI-Complete

--- Brain Jam/Assets/Main/Scripts/ChangeScene.cs
+++ Brain Jam/Assets/Main/Scripts/ChangeScene.cs
@@ -6,15 +6,58 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
 
+
+
   public void sceneSwap()
     {
 
         SceneManager.LoadScene(1);
       //SceneManager.LoadScene("maingame", LoadSceneMode.Additive);
     }
+public void timedSceneSwap(){
+
+  int time = 0;
+  bool act = true;
+  while (act){
+    time++;
+
+    if (time == 1000){
+
+      act =false;
+    }
+
+
+  }
+
+  SceneManager.LoadScene(1);
+
+
+
+}
+
 public void loadscene(){
 
-  SceneManager.LoadSceneAsync(1);
+  SceneManager.LoadScene(1);
+
+
+}
+
+
+public void levelSelectStarter(){
+SelectLevel s = new SelectLevel();
+
+  if ( s.getLevelnumber()== 1){
+
+    SceneManager.LoadScene(1);
+
+
+
+  }else {
+
+
+  }
+
+
 
 
 }
---
+++ Brain Jam/Assets/SelectLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectLevel : MonoBehaviour {
+  public Color color;
+	private static int levelnumber;
+public void select(int a){
+
+
+gameObject.GetComponent<Renderer>().material.color = color;
+
+levelnumber = a;
+
+
+}
+
+
+public int getLevelnumber(){
+
+
+	return levelnumber;
+}
+
+
+
+}
